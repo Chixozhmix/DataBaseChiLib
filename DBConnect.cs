9a6988eae7d4add2ba58be0b62e28e2bac5c9365
Chixozhmix <retrojas635@gmail.com>
2026-01-16 09:42:01 +0700

    Initial commit

---
+++ DBConnect.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DataBaseChiLib
+{
+    public class DBConnect
+    {
+        public SqlConnection SqlConnection;
+
+        public DBConnect(string serverName, string catalogName)
+        {
+            SqlConnection = new SqlConnection($@"Data Source={serverName};Initial Catalog={catalogName};Integrated Security=True");
+        }
+
+        public void openConnection()
+        {
+            if (SqlConnection.State == ConnectionState.Closed)
+            {
+                SqlConnection.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (SqlConnection.State == ConnectionState.Open)
+            {
+                SqlConnection.Close();
+            }
+        }
+
+        public SqlConnection getConnection()
+        {
+            return SqlConnection;
+        }
+    }
+}
